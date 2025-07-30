eaf9c0feba1e572721ac53c3fb26454472a269ff
peethree <pieterschoptkont@gmail.com>
2025-07-30 20:22:20 +0200

    changed no pairs from using employee.name to employee.id and added the conflict pairing constraint to the ortools function

--- main.cpp
+++ main.cpp
@@ -28,11 +28,11 @@ struct Employee {
 };
 
 struct No_pair {
-    std::string first;
-    std::string second;
+    int first;
+    int second;
 
     // constructor to initialize the fields
-    No_pair(const std::string& f, const std::string& s) : first(f), second(s) {}
+    No_pair(int f, int s) : first(f), second(s) {}
 };
 
 struct Target {
@@ -275,6 +275,102 @@ void assignEmployeesBalanced(std::vector<Distance>& distances, std::vector<Emplo
         std::cout << "no balanced solution found..." << std::endl;
     }
 }
+
+void assignEmployeesEnemies(std::vector<Distance>& distances, std::vector<Employee>& employees, std::vector<Target>& targets, std::vector<No_pair>& conflicts)
+{
+    int num_employees = employees.size();
+    int num_targets = targets.size();
+
+    MPSolver solver("EmployeeAssignment", MPSolver::SCIP_MIXED_INTEGER_PROGRAMMING);
+
+    // x[i][j] = 1 if employee i is assigned to target j
+    std::vector<std::vector<const MPVariable*>> x(num_employees, std::vector<const MPVariable*>(num_targets, nullptr));
+
+    // map employee ID to its index
+    std::unordered_map<int, int> id_to_index;
+    for (int i = 0; i < num_employees; ++i) {
+        id_to_index[employees[i].id] = i; 
+    }
+
+    // same for target (using .target_number instead of .id)
+    std::unordered_map<int, int> tar_num_to_index;
+    for (int i = 0; i < num_targets; ++i) {
+        tar_num_to_index[targets[i].target_number] = i; 
+    }
+
+    // create the decision variables
+    for (const auto& d : distances) {
+        int employee_index = id_to_index[d.employee.id];
+        int target_index = tar_num_to_index[d.target.target_number];
+
+        x[employee_index][target_index] = solver.MakeIntVar(0, 1, "x_" + std::to_string(employee_index) + "_" + std::to_string(target_index));
+    }
+
+    // constraint: each employee is assigned at most once
+    for (int i = 0; i < num_employees; ++i) {
+        LinearExpr expr;
+        for (int j = 0; j < num_targets; ++j) {
+            expr += x[i][j];
+        }
+        solver.MakeRowConstraint(expr <= 1);
+    }
+
+    // constraint: each target has a required number of people that need to be on location
+    for (int j = 0; j < num_targets; ++j) {
+        LinearExpr expr;
+        for (int i = 0; i < num_employees; ++i) {
+            expr += x[i][j];
+        }
+        solver.MakeRowConstraint(expr == targets[j].req_employees);
+    }
+
+    // constraint: some employees hate one another, don't pair them
+    for (const auto& conflict : conflicts) {
+        if (id_to_index.count(conflict.first) && id_to_index.count(conflict.second)) {
+            int first_employee = id_to_index[conflict.first];
+            int second_employee = id_to_index[conflict.second];
+
+            for (int k = 0; k < num_targets; ++k) {
+                if (x[first_employee][k] && x[second_employee][k]) {
+                    LinearExpr expr;
+                    expr += x[first_employee][k]; 
+                    expr += x[second_employee][k];
+                    solver.MakeRowConstraint(expr <= 1);
+                }
+            }
+        }
+    }
+
+    // objective: minimize the total distance
+    MPObjective* objective = solver.MutableObjective();
+    for (const auto& d : distances) {
+        int employee_index = id_to_index[d.employee.id];
+        int target_index = tar_num_to_index[d.target.target_number];
+
+        objective->SetCoefficient(x[employee_index][target_index], d.distance);
+    }
+    
+    objective->SetMinimization();
+
+    // solve
+    MPSolver::ResultStatus result_status = solver.Solve();
+
+    if (result_status == MPSolver::OPTIMAL) {
+        std::cout << "Optimal assignment found!" << std::endl;
+        for (const auto& d : distances) {
+            int employee_index = id_to_index[d.employee.id];
+            int target_index = tar_num_to_index[d.target.target_number];
+
+            if (x[employee_index][target_index]->solution_value() > 0.5) {
+                std::cout << "employee " << employees[employee_index].name << " assigned to Target:" << targets[target_index].target_number << " - "
+                          << targets[target_index].address << " (distance = " << d.distance << " km)" << std::endl;
+            }
+        }
+        std::cout << "total cost: " << objective->Value() << " km" << std::endl;
+    } else {
+        std::cout << "no optimal solution found..." << std::endl;
+    }
+}
 }
 
 int main(int argc, char** argv) {
@@ -325,13 +421,14 @@ int main(int argc, char** argv) {
                 for (const auto& other_emp : employees) {
                     if (p_name == other_emp.name) {
                         // std::cout << p_name << " found in no pair of : " << emp.name << std::endl;
-                        no_pairs.emplace_back(emp.name, other_emp.name);
+                        no_pairs.emplace_back(emp.id, other_emp.id);
                     }
                 }
             }
         }
     }
 
+    // these pairs ought not be at the same location:
     for (const auto& np : no_pairs) {
         std::cout << "(" << np.first << ", " << np.second << ")" << std::endl;
     }
@@ -457,7 +554,8 @@ int main(int argc, char** argv) {
         // TODO: based on command line input, run different function   
              
         // operations_research::assignEmployeesBalanced(distances, employees, targets);    
-        operations_research::assignEmployees(distances, employees, targets);       
+        // operations_research::assignEmployees(distances, employees, targets);       
+        operations_research::assignEmployeesEnemies(distances, employees, targets, no_pairs);
     }
 
     // TODO: add more fields to the json input files
